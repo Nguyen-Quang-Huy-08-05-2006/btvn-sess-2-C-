de1f3c1e8e6fdfead88bbfaaa8a654d378c129ff
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-20 23:55:35 +0700

    Add files via upload

---
+++ btvn6Sess2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float pi = 3.14;
+	int r = 5;
+
+    int chuVi = r*2*pi;
+    printf("chu vi = %d \n", chuVi);
+    int dienTich = r*r*pi;
+    printf("dien tich = %d \n", dienTich);
+    
+    return 0;
+}
