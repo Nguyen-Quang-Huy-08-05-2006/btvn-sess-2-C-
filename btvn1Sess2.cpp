2ab5f04f3b633d4e140571efe1f9555d6ae3f922
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-21 00:03:53 +0700

    Add files via upload

---
+++ btvn1Sess2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	//int la kieu du lieu so nguyen
+	int a = 5;
+	
+	//float la kieu du lieu so thuc
+	float a = 1.2;
+	
+	//double giong float nhung chinh xac hon
+	double a = 1.222222;
+	
+	//char la du lieu ky tu
+	char grade = 'A';
+
+}
