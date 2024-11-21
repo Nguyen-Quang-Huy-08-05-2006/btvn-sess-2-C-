409e13fe0d06799f55302f5f6bf61ee4af302777
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-21 11:18:05 +0700

    Update btvn5Sess2.cpp

--- btvn5Sess2.cpp
+++ btvn5Sess2.cpp
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main(){
 	int canhA = 5;
-	int canhB = 6;
+	int canhB = 5;
 	scanf("%d %d", &canhA, &canhB);
-	int chuVi = canhA + canhB;
+	int chuVi = 2 * (canhA + canhB);
 	printf("chu vi = %d %d \n", chuVi);
 	int dienTich = canhA * canhB;
 	printf("dien tich = %d %d \n", dienTich);
 	
-	return 0;
-
-	
-}
+	return0;
