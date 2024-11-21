841d3837be6240a008be9410b8771ec0dc372edd
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-21 11:19:14 +0700

    Update btvn2Sess2.cpp

--- btvn2Sess2.cpp
+++ btvn2Sess2.cpp
@@ -4,12 +4,6 @@ int main(){
 	int num2 = 100;
 	int tong = num1 + num2;
 	printf("tong = %d \n", tong);
-	int tich = num1 * num2;
-	printf("tich = %d \n", tich);
-	int hieu = num1 - num2;
-	printf("hieu = %d \n", hieu);
-	int thuong = num1 / num2;
-	printf("thuong = %d \n", thuong);
 	
 	return 0;
 
